2b1bb90e6284489f7b2667bf74bc597610684110
pangastika <pangastikadian@gmail.com>
2016-12-06 10:32:25 +0700

    versi 1.0.0

---
+++ app/models/book.rb
@@ -0,0 +1,8 @@
+class Book < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :category
+  has_many :reviews
+  
+  has_attached_file :book_img, styles: { book_index: "250x350>", book_show: "325x475>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :book_img, content_type: /\Aimage\/.*\z/
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  Paperclip.options[:command_path] = "/usr/local/bin/"
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+
 end
